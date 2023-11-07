d8b9b78d412f0e9245b75dfca58171bf48557310
Татьяна Хонякина <hanakinat@gmail.com>
2023-11-07 12:38:14 +0300

    Верстка приложения

---
+++ src/views/GameView.vue
@@ -0,0 +1,183 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+    <div class="header">
+      <button class="cancel"><img src="/icons/cancel.svg" alt="">Отмена</button>
+      <div class="timer">
+        <p class="title_timer">4:37</p>
+      </div>
+    </div>
+    <div class="example">
+      <p>13 * <input class="hidden" type="text"> * <input class="hidden" type="text"></p>
+      <p class="correct_result">= 84240?</p>
+    </div>
+    <div class="actions">
+      <div class="actions_number">
+        <button class="number">1</button>
+        <button class="number">2</button>
+        <button class="number">3</button>
+        <button class="number">4</button>
+        <button class="number">5</button>
+        <button class="number">6</button>
+        <button class="number">7</button>
+        <button class="number">8</button>
+        <button class="number">9</button>
+        <div></div>
+        <button class="number">0</button>
+      </div>
+      <div class="actions_users">
+        <button class="action"><img src="/icons/arrow_back.svg" alt=""></button>
+        <button class="action"><img src="/icons/arrow_forward.svg" alt=""></button>
+        <button class="action"> ?</button>
+        <button class="action"> =</button>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+img {
+  height: 15px;
+}
+
+main {
+  width: 400px;
+}
+
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.cancel {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 92px;
+  height: 34px;
+  text-transform: uppercase;
+  background: transparent;
+  border: 1px solid #9f9f9f;
+  box-shadow: 0 2px 2px 0 rgba(148, 148, 148, 0.53);
+
+  & img {
+    margin-right: 5px;
+  }
+}
+
+.title_timer {
+  margin: 0;
+}
+
+.timer {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 60px;
+  height: 30px;
+  border: 2px solid #5395e7;
+  border-right: 20px solid #5395e7;
+  background-color: #f5f5f5;
+  padding-right: 10px;
+}
+
+.example {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 20px;
+}
+
+.correct_result {
+  margin: 0;
+  color: #bfbfbf;
+  font-weight: 600;
+}
+
+.hidden {
+  background: transparent;
+  border: transparent;
+  border-bottom: 1px solid #bfbfbf;
+  color: black;
+  font-size: 16px;
+  max-width: 30px;
+  text-align: center;
+  font-weight: 600;
+}
+
+p {
+  font-weight: 600;
+}
+
+.hidden:focus {
+  outline: none;
+  border-bottom: 1px solid black;
+}
+
+.actions {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
+}
+
+.actions_number {
+  display: grid;
+  grid-template-columns: repeat(3, 40px);
+  gap: 25px;
+  margin-right: 25px;
+}
+
+.number {
+  background-color: #f7724d;
+  width: 50px;
+  height: 50px;
+  border: transparent;
+  border-radius: 50%;
+  box-shadow: 0 4px 4px 0 rgba(150, 145, 145, 0.53);
+}
+
+.number:hover {
+  background-color: #de613f;
+  cursor: pointer;
+  transition: 0.2s;
+  color: #ffffff;
+}
+
+.actions_users {
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+}
+
+.action {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #797979;
+  width: 50px;
+  height: 50px;
+  border: transparent;
+  border-radius: 50%;
+  box-shadow: 0 4px 4px 0 rgba(150, 145, 145, 0.53);
+}
+
+.action:hover {
+  background-color: #8a8a8a;
+  cursor: pointer;
+  transition: 0.2s;
+  color: #ffffff;
+}
+
+.action,
+.number {
+  font-family: "Nunito Regular", sans-serif;
+  font-weight: 600;
+}
+</style>
---
+++ src/views/SettingView.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+
+import {ref} from "vue";
+
+const dur = ref(0);
+const com = ref(0);
+
+</script>
+
+<template>
+  <div class="setting">
+    <h1 class="greeting">Привет!</h1>
+    <div class="result">
+      <p>Добро пожаловать на 24 тренировочный день,</p>
+      <p>Ваш последний результат - решено 10 из 25.</p>
+      <p>Общая точность 80%.</p>
+    </div>
+    <div class="setting_game">
+      <h1 class="setting_title">Настройки</h1>
+      <div class="setting_duration">
+        <input type="range" min="1" max="15" v-model="dur">
+        <p class="duration">Длительность {{ dur }}</p>
+      </div>
+      <div class="setting_complexity">
+        <input type="range" min="1" max="10" v-model="com">
+        <p class="complexity">Сложность {{ com }}</p>
+      </div>
+      <div class="math_operation">
+        <div><input type="checkbox">Суммирование</div>
+        <div><input type="checkbox">Разность</div>
+        <div><input type="checkbox">Умножение</div>
+        <div><input type="checkbox">Деление</div>
+        <div><input type="checkbox">Возведение в степень</div>
+      </div>
+    </div>
+    <button class="go">Начать!</button>
+  </div>
+</template>
+
+<style scoped>
+
+input {
+  margin-right: 15px;
+}
+
+.setting {
+  width: 400px;
+}
+
+.setting_title {
+  font-size: 22px;
+}
+
+.result {
+  & p {
+    margin: 0;
+  }
+}
+
+.go {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 92px;
+  height: 34px;
+  text-transform: uppercase;
+  background: transparent;
+  border: 1px solid #9f9f9f;
+  box-shadow: 0 2px 2px 0 rgba(148, 148, 148, 0.53);
+  margin-left: auto;
+}
+
+.complexity,
+.duration {
+  margin-top: 0;
+}
+
+@media (min-width: 1024px) {
+  .setting {
+    min-height: 100vh;
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+  }
+
+  .math_operation {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+}
+</style>
